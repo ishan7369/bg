b5de13d01db2d361dfd16d225993e869d452582a
Ishan Keshav <109731292+ishan7369@users.noreply.github.com>
2025-03-12 01:52:41 +0530

    Update server.py

--- server.py
+++ server.py
@@ -232,7 +232,7 @@ def handle_bgm(message):
             target = command[1]
             port = int(command[2])  # Convert time to integer
             time = int(command[3])  # Convert port to integer
-            if time > 181:
+            if time > 581:
                 response = "Error: Time interval must be less than 80."
             else:
                 record_command_logs(user_id, '/bgm', target, port, time)
